83909f1138bb7f5177b1c591adac45b56a5f3e4e
seahsongli <seahsongli@gmail.com>
2024-09-22 01:05:30 +0800

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WorldcoinVerification from './verify';
+
+function App() {
+  return (
+    <div className="App">
+      <WorldcoinVerification/>
+    </div>
+  );
+}
+
+export default App;
