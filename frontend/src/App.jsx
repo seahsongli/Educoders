65603c998cb4ffdb6c1b559e71f99eb5e687b3bc
jarrellc1 <Jarrell.chia@gmail.com>
2024-09-22 03:46:09 +0800

    For songli

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,28 @@ import './App.css';
 import WorldcoinVerification from './verify';
 import { useState } from 'react';
 import HomePage from './home';
+import HelpPage from './HelpPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [verification, setVerification] = useState(false);
+
   const handleVerifySuccess = () => {
     setVerification(true);
   };
+
   return (
-    <div className="App">
-      {!verification && <WorldcoinVerification onVerifySuccess={handleVerifySuccess}/>}
-      {verification && <HomePage />}
-    </div>
+    <Router>
+      <div className="App">
+        {!verification && <WorldcoinVerification onVerifySuccess={handleVerifySuccess} />}
+        {verification && (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/help" element={<HelpPage />} />
+          </Routes>
+        )}
+      </div>
+    </Router>
   );
 }
 
